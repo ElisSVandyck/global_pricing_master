891d32caf61f2fc2fed87615533858bb10ae417e
Elis Vandyck <elissvandyck@gmail.com>
2022-08-05 23:38:47 +0100

    update food codes
    update food codes

--- booking/booking_scraper.py
+++ booking/booking_scraper.py
@@ -2,11 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import requests, json
 from bs4 import BeautifulSoup
 import re
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -15,5 +13,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
-
 import pyperclip
+
+driver = webdriver.Chrome( service = Service(ChromeDriverManager().install()))
+
+
+
+
+actions = ActionChains(driver)
+actions.send_keys(Keys.RETURN)
+actions.perform()
+
+actions = ActionChains(driver)
+
+actions.key_down(Keys.CONTROL)
+actions.send_keys("a")
+actions.key_up(Keys.CONTROL)
+actions.perform()
+
+actions.key_down(Keys.CONTROL)
+actions.send_keys("c")
+actions.key_up(Keys.CONTROL)
+actions.perform()
+
+page_text = pyperclip.paste()
+
+
+
+
+
+
